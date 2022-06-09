8121cc7225455a048d977069f6cd0686cd68df26
zeynab66 <alizadeh.zeynab66@gmail.com>
2022-06-09 15:54:21 -0700

    first commit

---
+++ craco.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  style: {
+    ProcessingInstructionss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResult from "../components/users/UserResult";
+import UserSearch from "../components/users/UserSearch";
+function Home() {
+  return (
+    <>
+      <UserSearch />
+      <UserResult />
+    </>
+  );
+}
+
+export default Home;
